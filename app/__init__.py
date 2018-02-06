7c3fb443575ba5d7e1608b79c158a5a6e7b8e2fe
ennl2py <811501726@qq.com>
2018-02-06 23:36:34 +0800

    优化1a内容

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
-bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -11,7 +9,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    bootstrap.init_app(app)
     db.init_app(app)
 
     # 注册前台蓝本
